425254d0b0ccdd06543a7d12709f3336c815aa80
Adekunle Oduye <adekunleoduye@gmail.com>
2019-04-25 10:47:51 -0400

    second iteration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import { graphql } from "gatsby"
+// import Button from '../components/Button'
 
 import Layout from "../components/layout"
 
 const IndexPage = ({data}) => (
   <Layout>
-     <div>
-       {data.allSuperHeroesJson.edges.map(hero => {
-         
-         return <img src={hero.node.img} alt={hero.node.name}/>
+    <div class="heroes-group font">
+   
+       {data.allSuperHeroesJson.edges.map((hero,i) => {
+
+         return (
+          <div class="heroes-item" key={i}>
+              <h3>
+                {hero.node.hero}
+              </h3>
+              
+              <img src={hero.node.img} class="mb0"/>
+              <a href={hero.node.link} class="btn btn-primary full-width">Link to profile</a>
+            </div>)
         })}
+  
     </div>
-    
+     
   </Layout>
 )
 
@@ -21,7 +32,10 @@ export const query = graphql`
       edges {
         node {
           name
+          hero
+          link
           img
+          bio
         }
       }
     }
